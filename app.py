47758510326792333c5373078e0a28aac435adfa
carefreecherry <63678941+carefreecherry@users.noreply.github.com>
2024-11-15 03:29:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import blog, login_signup, chatbot, userprofile, personalized_plan, dashboard, schedules, depression, iesr, ptsd, EmpathAI2
+import blog, login_signup, chatbot, userprofile, personalized_plan, dashboard, schedules, depression, empathAI, iesr, ptsd, doctor_profile, doctor_patients
 import streamlit.components.v1 as components 
 
 def open_external_link(url):
@@ -32,39 +32,66 @@ def main():
     st.sidebar.title("Menu")
 
     if st.session_state['logged_in']:
-        # Logged-in menu
-        menu = ["Profile", "Dashboard", "Personalized Plan", "Schedules", "Create Blog", "View Blogs", "Chatbot", "CBT_Depression", "CBT_IES-R", "CBT_PTSD", "Granny", "Open Workout Assistant", "Logout"]
-        choice = st.sidebar.radio("Navigation", menu, key='logged_in_menu')
+        user_type = st.session_state.get('user_type', 'patient')
 
-        if choice == "Profile":
-            userprofile.profile_page()
-        elif choice == "Dashboard":
-            dashboard.dashboard_page()
-        elif choice == "Personalized Plan":
-            personalized_plan.personalized_plan_page()
-        elif choice == "Schedules":
-            schedules.schedules_page()
-        elif choice == "Create Blog":
-            blog.create_blog()
-        elif choice == "View Blogs":
-            blog.view_blogs()
-        elif choice == "Chatbot":
-            chatbot.chatbot_page()
-        elif choice == "CBT_Depression":
-            depression.page()
-        elif choice == "CBT_IES-R":
-            iesr.page()
-        elif choice == "CBT_PTSD":
-            ptsd.page()
-        elif choice == "Granny":
-            EmpathAI2.page()
-        elif choice == "Open Workout Assistant":
-            open_external_link("http://localhost:8080/")
-        elif choice == "Logout":
-            st.session_state['logged_in'] = False
-            st.session_state['user_id'] = None
-            st.session_state['username'] = None
-            st.success("You have been logged out.")
+        if user_type == 'patient':
+            # Logged-in menu
+            menu = ["Profile", "Dashboard", "Doctors", "Personalized Plan", "Schedules", "Create Blog", "View Blogs", "Health Guru", "GrandmaBot","CBT_Depression", "CBT_IES-R", "CBT_PTSD", "Open Workout Assistant", "Logout"]
+            choice = st.sidebar.radio("Navigation", menu, key='logged_in_menu')
+
+            if choice == "Profile":
+                userprofile.profile_page()
+            elif choice == "Dashboard":
+                dashboard.dashboard_page()
+            elif choice == "Doctors":
+                userprofile.manage_doctor_requests()
+            elif choice == "Personalized Plan":
+                personalized_plan.personalized_plan_page()
+            elif choice == "Schedules":
+                schedules.schedules_page()
+            elif choice == "Create Blog":
+                blog.create_blog()
+            elif choice == "View Blogs":
+                blog.view_blogs()
+            elif choice == "Health Guru":
+                chatbot.chatbot_page()
+            elif choice == "GrandmaBot":
+                empathAI.page()
+            elif choice == "CBT_Depression":
+                depression.page()
+            elif choice == "CBT_IES-R":
+                iesr.page()
+            elif choice == "CBT_PTSD":
+                ptsd.page()
+            elif choice == "Open Workout Assistant":
+                open_external_link("http://localhost:8080/")
+            elif choice == "Logout":
+                st.session_state['logged_in'] = False
+                st.session_state['user_id'] = None
+                st.session_state['username'] = None
+                st.session_state['user_type'] = None
+                st.success("You have been logged out.")
+        elif user_type == 'doctor':
+            # Doctor menu
+            menu = ["Profile", "Patients", "Create Blog", "View Blogs", "Chatbot", "Logout"]
+            choice = st.sidebar.radio("Navigation", menu)
+
+            if choice == "Profile":
+                doctor_profile.doctor_profile()
+            elif choice == "Patients":
+                doctor_patients.doctor_patients_page()
+            elif choice == "Create Blog":
+                blog.create_blog()
+            elif choice == "View Blogs":
+                blog.view_blogs()
+            elif choice == "Chatbot":
+                chatbot.chatbot_page()
+            elif choice == "Logout":
+                st.session_state['logged_in'] = False
+                st.session_state['user_id'] = None
+                st.session_state['username'] = None
+                st.session_state['user_type'] = None
+                st.success("You have been logged out.")
     else:
         # Not logged-in menu
         menu = [
